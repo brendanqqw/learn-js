da79f291499586187b212dbf923e4dcd8d88bb09
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-19 08:08:59 +0000

    v3.3

---
+++ LearnJS3/js/challenge.js
@@ -0,0 +1,9 @@
+let math = 90;
+let english = 88;
+let science = 93;
+let filipino = 95;
+let mapeh = 85;
+
+
+let average = (math + english + science + filipino + mapeh) /5;
+alert("Your Average is : " + average);
