a25d669c0517b83cb9aedc9cb7d04d5d7a63b611
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-21 08:44:58 +0000

    v4.6

--- LearnJS4/js/challenge.js
+++ LearnJS4/js/challenge.js
@@ -3,4 +3,4 @@ let name2 = "David";
 let word1 = "she";
 let word2 = "coffee";
 
-alert(`${name1}, the friendly neighbor. waved at ${name2} as she walked by ${name2}'s house. ${name2} smiled back and invited ${name1} in for a cup of ${word2}.`)
+alert(`${name1}, the friendly neighbor. waved at ${name2} as she walked by ${name2}'s house. ${name2} smiled back and invited ${name1} in for a cup of ${word2}.`);
