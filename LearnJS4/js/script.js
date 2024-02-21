a22d115901ad301a326db9d46ec14df500f03f57
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-21 08:56:33 +0000

    v4.7

--- LearnJS4/js/script.js
+++ LearnJS4/js/script.js
@@ -1,4 +1,6 @@
-let num1 = 5;
-let num2 = 3;
-alert(`${num1} + ${num2} = ${num1+num2}`);
+let name1 = "Alenere";
+let name2 = "David";
+let word1 = "she";
+let word2 = "coffee";
 
+alert(`${name1}, the friendly neighbor, waved at ${name2} as she walked by ${name2}'s house. ${name2} smiled back and invited ${name1} in for a cup of ${word2}.`);
