9d79108d26589117a5e4463aa99103a704902ce6
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-22 19:53:27 +0800

    v5.3

--- LearnJS5Arrays/js/script.js
+++ LearnJS5Arrays/js/script.js
@@ -1,2 +1,12 @@
-let names = ["Brendan", "Raul", "Jomar", "Jasper"];
-alert(names[2]);
+let cars = ["Yaris", "Innova"];
+
+//Add using LENGTH
+cars[cars.length] = "Sentra";
+
+//Add using push(value)
+cars.push("Juke");
+
+//Add using unshift(value)
+cars.unshift("Lancer");
+
+alert(cars);
