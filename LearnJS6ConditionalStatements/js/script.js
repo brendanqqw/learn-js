145c018c0770f6edf25d0df122576601e247f91f
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-23 07:08:43 +0000

    v6.1

---
+++ LearnJS6ConditionalStatements/js/script.js
@@ -0,0 +1,13 @@
+console.log(5 == "5");
+//Explanation is 5 equal to 5 answer: true
+//Output: true
+
+console.log(5 === "5");
+//Explanation is 5 straight equal to 5? the answer is false because 5 is not a number its a string so the answer is false
+//Output: false
+
+console.log(5 != "5");
+//Output: false
+
+console.log(5 !== "5");
+//Output: true
