abcd6e8220f3ea0d5e410c242db833c9ab0ec1c2
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-23 06:45:50 +0000

    v5.6

---
+++ LearnJS5Arrays/js/challenge.js
@@ -0,0 +1,16 @@
+let firstNames = ["Brendan", "Brandon", "Raul"];
+let lastNames = ["Degrano", "Degano", "Degz"];
+let age = [22,16,50];
+
+let id = 2;
+
+console.log(`ID        :  ${id}`);
+console.log(`First Name:  ${firstNames[id]}`);
+console.log(`Last Name :  ${lastNames[id]}`);
+console.log(`Age       :  ${age[id]}`);
+
+//Output:
+//ID        : 0
+//First Name: Brendan
+//Last Name : Degrano
+//Age       : 22
