e8c758f7e612ac0388ad0120b4dc4f704e64a56f
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-23 06:08:12 +0000

    v5.4

--- LearnJS5Arrays/js/script.js
+++ LearnJS5Arrays/js/script.js
@@ -1,5 +1,4 @@
-let names = ["David", "Alenere"];
-names[names.length] = "Jaymar";
-names[names.length] = "Maye";
-alert(names);
-//Output: David,Alenere,Jaymar,Maye
+let names = ["Alenere", "David", "Jaymar", "Maye". "Ace"];
+
+//DELETE using LENGTH
+names.length = 4;
