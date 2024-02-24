063f472820a3a1f125ac52766c15d945a5a30140
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-24 15:42:57 +0800

    v6.3

--- LearnJS6ConditionalStatements/js/script.js
+++ LearnJS6ConditionalStatements/js/script.js
@@ -1,18 +1,14 @@
-let x = 15;
-let y = 15;
+let age = 11;
 
-alert(x > y);
-// Output: false
-// Explanation: The value of x is 12 and the value of y is "15". The greater than operator (>) compares the values of x and y. Since the value of x is less than the value of y, the output is false.
+if (age >= 18) {
+  console.log("Legal Age");
+} else if (age <= 0) {
+  console.log("Invalid Age");
+} else {
+  console.log("Minor Age");
+}
 
-alert(x < y);
-// Output: true
-// Explanation: The value of x is 12 and the value of y is "15". The less than operator (<) compares the values of x and y. Since the value of x is less than the value of y, the output is true.
+console.log("Tnak you!");
 
-alert(x >= y);
-// Output: false
-// Explanation: The value of x is 12 and the value of y is "15". The greater than or equal to operator (>=) compares the values of x and y. Since the value of x is less than the value of y, the output is false.
-
-alert(x <= y);
-// Output: true
-// Explanation: The value of x is 12 and the value of y is "15". The less than or equal to operator (<=) compares the values of x and y. Since the value of x is less than the value of y, the output is true.
+//Output: Invalid Age
+//        Tnak you!
