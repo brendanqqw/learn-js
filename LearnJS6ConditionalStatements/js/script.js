b0a87c58647d64f167075e77d4d553311072d665
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-24 12:06:23 +0800

    v6.1

--- LearnJS6ConditionalStatements/js/script.js
+++ LearnJS6ConditionalStatements/js/script.js
@@ -7,7 +7,26 @@ console.log(5 === "5");
 //Output: false
 
 console.log(5 != "5");
+//Explanation is 5 not equal to 5? the answer is false because 5 is equal to 5
 //Output: false
 
 console.log(5 !== "5");
+//Explanation is 5 not straight equal to 5? the answer is true because 5 is not a number its a string so the answer is true
+//Output: true
+
+let x = 5;
+console.log(x == "5");
+//Explanation is x which is 5 equal to 5? answer: true
+//Output: true
+
+console.log(x === "5");
+//Explanation is x which is 5 straight equal to 5? the answer is false because 5 is not a number its a string so the answer is false
+//Output: false
+
+console.log(x != "5");
+//Explanation is x which is 5 not equal to 5? the answer is false because 5 is equal to 5
+//Output: false
+
+console.log(x !== "5");
+//Explanation is x which is 5 not straight equal to 5? the answer is true because 5 is not a number its a string so the answer is true
 //Output: true
