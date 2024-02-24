cd190f4ed23010b8195d544a534a1fad94bc61f2
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-24 17:25:25 +0800

    v7.5

--- LearnJS7LogicalOperators/js/script.js
+++ LearnJS7LogicalOperators/js/script.js
@@ -1,13 +1,15 @@
-//NOT OPERATOR
-//INVERTS the result of a CONDITION
+//NESTED Conditional Statements
+//You can nest conditional statements inside a conditional statement
 
-let hasDegree = true;
-let experience = 3;
+let age = 16;
+let experience = 1;
 
-console.log(!hasDegree);
-//Output: false
-//Explanation: The value of hasDegree is true, but the NOT operator inverts the result of the condition, so the output is false.
-
-console.log(!(experience > 1));
-//Output: false
-//Explanation: The value of experience is 3, and the condition is true, but the NOT operator inverts the result of the condition, so the output is false.
+if (age >= 18) {
+  if (experience >= 1) {
+    console.log("You are hired!");
+  } else {
+    console.log("You do not have enough experience");
+  }
+} else {
+  console.log("You are not old enough to work");
+}
