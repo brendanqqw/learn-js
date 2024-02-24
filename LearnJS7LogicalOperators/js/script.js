c85de213bc30bd41a67fa9db51a177e6e20da652
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-24 16:14:03 +0800

    v7.0

---
+++ LearnJS7LogicalOperators/js/script.js
@@ -0,0 +1,10 @@
+let age = 18;
+let experience = 3;
+
+console.log(age >= 18 && experience > 1);
+//Output: true
+//Explanation: Both conditions are true, so the result is true.
+
+console.log(age >= 18 && experience > 3);
+//Output: false
+//Explanation: The first condition is true, but the second condition is false, so the result is false.
