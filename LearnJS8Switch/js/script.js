cc784005f84eeb447ce778370f2eb8f52024181e
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-25 19:44:15 +0800

    v8.0

---
+++ LearnJS8Switch/js/script.js
@@ -0,0 +1,14 @@
+//Example
+let difficulty = 3;
+
+switch (difficulty) {
+  case 1:
+    console.log("Easy");
+    break;
+  case 2:
+    console.log("Normal");
+    break;
+  case 3:
+    console.log("Hard");
+    break;
+}
