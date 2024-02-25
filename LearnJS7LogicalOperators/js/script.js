f31071f5a2fffd70781ce293ef8cb979c931e879
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-25 18:47:02 +0800

    v7.7

--- LearnJS7LogicalOperators/js/script.js
+++ LearnJS7LogicalOperators/js/script.js
@@ -1,11 +1,14 @@
 //NESTED Conditional Statements
 //You can nest conditional statements inside a conditional statement
 
-let age = 16;
-let experience = 1;
+let age = 20;
+let experience = 5;
 
 if (age >= 18) {
-  if (experience >= 1) {
+  if (experience > 1) {
+    if (experience >= 5) {
+      console.log("You are overqualified!");
+    }
     console.log("You are hired!");
   } else {
     console.log("You do not have enough experience");
