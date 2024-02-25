63bef5975491258e3862a63db8c65076b595c754
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-25 19:39:04 +0800

    v7.7

--- LearnJS7LogicalOperators/js/challenge.js
+++ LearnJS7LogicalOperators/js/challenge.js
@@ -2,10 +2,20 @@
 //Not Registered and 18 above - Register First
 // Registered and below 18 - Invalid Voter
 //Not Registered and below 18 - Non Voter
-let age = 16;
+let age = 11;
 let isRegistered = false;
 
-if (isRegistered && age >= 18) alert("Valid Voter");
-else if (!isRegistered && age >= 18) alert("Register First");
-else if (isRegistered && age < 18) alert("Invalid Voter");
-else if (!isRegistered && age < 18) alert("Non Voter");
+//if (isRegistered && age >= 18) alert("Valid Voter");
+//else if (!isRegistered && age >= 18) alert("Register First");
+//else if (isRegistered && age < 18) alert("Invalid Voter");
+//else if (!isRegistered && age < 18) alert("Non Voter");
+
+if (isRegistered) {
+  if (age >= 18) alert("Valid Voter");
+  else alert("Invalid Voter");
+} else {
+  if (age >= 18) alert("Register First");
+  else {
+    alert("Non Voter");
+  }
+}
