3cee81c5166e0bfc4965631d62e6faa51a087862
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-26 08:08:33 +0000

    v10.5

--- LearnJS10FORLoopInOf/js/challenge.js
+++ LearnJS10FORLoopInOf/js/challenge.js
@@ -1,11 +1,17 @@
-let names = ["Brendan", "Raul", "Basil", "Degrano"]; // Array of people
-let search = "a"; // Search for this name
+let people = ["Brendan", "Raul", "Basil", "Degrano"]; // Array of people
+let search = "bAsIl"; // Search for this name
+let isFound = false; // Flag to check if the name is found
 
-for(let name of names){ // Loop through the array
-    if(name === search){ // Check if the name is found
-        console.log(`${search} is found in the array`); // Print the result
-    }else{
-        console.log(`${search} is not found in the array`); // Print the result
+for (let i = 0; i < people.length; i++) { // Loop through the array
+    if (people[i].toLowerCase() === search.toLowerCase()) { // Check if the name is found
+        isFound = true; // Set the flag to true
+        console.log(`Found ${people[i]}`); // Print the result
+        break; // Break the loop
     }
-    break; // Break the loop
 }
+
+if (!isFound) { // Check if the flag is false
+    console.log("Not Found"); // Print the result
+}
+
+//output: Found Basil
--- LearnJS10FORLoopInOf/js/script.js
+++ LearnJS10FORLoopInOf/js/script.js
@@ -1,9 +1,17 @@
 let people = ["Brendan", "Raul", "Basil", "Degrano"]; // Array of people
 let search = "bAsIl"; // Search for this name
+let isFound = false; // Flag to check if the name is found
 
-for (let person = 0; i < people.length; i++) { // Loop through the array
-    if (people[person].toLowerCase() === search.toLowerCase()) { // Check if the name is found
-        console.log(`${search} is found in the array`); // Print the result
+for (let i = 0; i < people.length; i++) { // Loop through the array
+    if (people[i].toLowerCase() === search.toLowerCase()) { // Check if the name is found
+        isFound = true; // Set the flag to true
+        console.log(`Found ${people[i]}`); // Print the result
         break; // Break the loop
     }
 }
+
+if (!isFound) { // Check if the flag is false
+    console.log("Not Found"); // Print the result
+}
+
+//output: Found Basil
