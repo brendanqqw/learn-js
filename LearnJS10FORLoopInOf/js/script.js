c139133c437ef90a88736dcdc805f41260772a8b
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-26 07:47:54 +0000

    v10.5

--- LearnJS10FORLoopInOf/js/script.js
+++ LearnJS10FORLoopInOf/js/script.js
@@ -8,3 +8,12 @@ for(let person of people){ // Loop through the array
 //         Alenere
 //         Gibert
 //         James
+
+for(let person in people){ // Loop through the array
+    console.log(`${parseInt(person)+1}. ${people[person]}`); // Print Index
+}
+
+// Output: 1. David
+//         2. Alenere
+//         3. Gibert
+//         4. James
