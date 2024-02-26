65811664636548da350b526f5f3cbc9a082bb5d6
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-26 19:50:19 +0800

    v11.0

--- LearnJS11JSON/js/script.js
+++ LearnJS11JSON/js/script.js
@@ -2,17 +2,8 @@ let person = {
   firstName: "John",
   lastName: "Doe",
   age: 25,
-  hobbies: ["coding", "eating", "sleeping"],
-  pets: {
-    0: {
-      name: "Rex",
-      type: "Dog",
-    },
-    1: {
-      name: "Fluffy",
-      type: "Cat",
-    },
-  },
 };
 
-console.log(person);
+//Only Works on a "String" Key
+console.log(`${person.firstName} ${person.lastName}`);
+//Output: John Doe
