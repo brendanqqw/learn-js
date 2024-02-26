3ce283f84b5b02777518495c3c6fb6d4083b6f8f
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-26 05:57:13 +0000

    v9.1

--- LearnJS9WhileDoLoop/js/script.js
+++ LearnJS9WhileDoLoop/js/script.js
@@ -1,12 +1,9 @@
 let people = ["David", "Alenere", "Gibert", "James"]; //array of people
-let i = 0; //index
+let i = people.length-1; //index of the last person in the array
 
 while(people[i]){ //while there is a person in the array
     console.log(people[i]); //print the person
-    i++; //increment the index
+    i--; //decrement the index 
 }
 
-//output: David
-//        Alenere
-//        Gibert
-//        James
+
