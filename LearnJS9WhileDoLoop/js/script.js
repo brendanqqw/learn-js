f64177f3babf9523d7eef17f2ccbec0422c267ef
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-26 06:10:28 +0000

    v9.4

--- LearnJS9WhileDoLoop/js/script.js
+++ LearnJS9WhileDoLoop/js/script.js
@@ -1,16 +1,13 @@
 let people = ["David", "Alenere", "Gibert", "James"]; //array of people
 let i = 0; //index of the array
+let search = "Gibert"; //search for Gibert in the array
 
-while(people[i]){
-    console.log(people[i]); //print the people in the array
+while(people[i]){ //while there is a person in the array
+    if(people[i] === search){ //if the person is Gibert
+        console.log(people[i]); //print Gibert
+        break; //stop the loop
+    }
     i++; //increment the index
- //while loop
-
-  if(i === 2){ //if the index is 2
-    break; //break the loop
-  }
 }
 
-//output: David
-//        Alenere
-
+//Output: Gibert
