ea0a4e733627d66d9fdf728fed127fa1f2c9b11e
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-26 05:47:11 +0000

    v9.0

---
+++ LearnJS9/js/script.js
@@ -0,0 +1,12 @@
+let i = 0;
+
+while (i < 5) {
+  console.log(i);
+  i++; //Why i++? It's because if we don't increment i, the loop will run forever.
+}
+
+// Output:  0
+//          1
+//          2
+//          3
+//          4
