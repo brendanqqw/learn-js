e9c8032d9bc0c9feb7a7c01e99f8dc20cfeef678
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-26 06:19:29 +0000

    v9.5

--- LearnJS9WhileDoLoop/js/script.js
+++ LearnJS9WhileDoLoop/js/script.js
@@ -2,4 +2,7 @@ let i = 0;
 
 do{ // do while loop
   console.log("Hello World"); // print "Hello World"
+  i++; // increment i by 1
 }while(i > 5); // condition is false, so it will print "Hello World" once
+
+//Output: Hello World
