27d8c97c773523d09725f9e4828ec8aaffeb2548
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-26 07:29:31 +0000

    v10.5

--- LearnJS10FORLoopInOf/js/script.js
+++ LearnJS10FORLoopInOf/js/script.js
@@ -1,12 +1,11 @@
 let people = ["David", "Alenere", "Gibert", "James"]; // Array of people
 
-for(let i = 0; i < people.length; i++) { // Loop through the array
+for(let i in people) { // Loop through the array
     console.log(people[i]); // Print the name of the person
-
-    if( i === 1) { // If the index is 1
-        break; // Stop the loop
-    }
+    break; // Break the loop
 }
 
 // Output: David
 //         Alenere
+//         Gibert
+//         James
