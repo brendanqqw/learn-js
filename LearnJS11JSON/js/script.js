59f53451060ab8c22a12bc5cf613c55a9163b2c8
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-26 19:37:29 +0800

    v11.0

--- LearnJS11JSON/js/script.js
+++ LearnJS11JSON/js/script.js
@@ -1,6 +1,18 @@
-//JSON Example
 let person = {
   firstName: "John",
   lastName: "Doe",
   age: 25,
+  hobbies: ["coding", "eating", "sleeping"],
+  pets: {
+    0: {
+      name: "Rex",
+      type: "Dog",
+    },
+    1: {
+      name: "Fluffy",
+      type: "Cat",
+    },
+  },
 };
+
+console.log(person);
