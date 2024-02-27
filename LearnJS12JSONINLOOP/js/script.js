27c82adee1f961020fcff6b0dbe10de228a04c59
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-27 15:45:24 +0800

    v12.3

--- LearnJS12JSONINLOOP/js/script.js
+++ LearnJS12JSONINLOOP/js/script.js
@@ -4,7 +4,5 @@ let person = {
   age: 22,
 };
 
-let keys = Object.keys(person);
-for (let i = 0; i < keys.length; i++) {
-  console.log(person[keys[i]]);
-}
+let size = Object.keys(person).length; //Explanation: Object.keys() returns an array of a given object's own property names, in the same order as we get with a normal loop. We can use it to get the length of an object. In this case, we are getting the length of the person object.
+console.log(size); //Output: 3
