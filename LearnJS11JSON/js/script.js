3fe8c5b4457b8522ee43c9a7d800e71eaede535b
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-27 13:48:28 +0800

    v11.5

--- LearnJS11JSON/js/script.js
+++ LearnJS11JSON/js/script.js
@@ -2,20 +2,7 @@ let person = {
   firstName: "John",
   lastName: "Doe",
   age: 25,
-  hobbies: ["reading", "coding", "swimming"],
 };
 
-//Update Existing "key | value"
-person["firstName"] = "Brendan"; //Explanation: adds a new key | value to the object
-//person.firstName = "Brendan"; Same Concept
-console.log(person); //Output: {firstName: "Brendan", lastName: "Doe", age: 25}
-
-//Create New Key | Value"
-person["middleInitial"] = "B"; //Explanation: adds a new key | value to the object
-person.middleInitial = "B"; // Same Concept
-console.log(person); //Output: {firstName: "Brendan", lastName: "Doe", age: 25, middleInitial: "B"}
-
-console.log(person.hobbies); //Output: ["reading", "coding", "swimming"]
-person.hobbies[0] = "Writing";
-//person.hobbies[0] = "Playing" //Same Concept
-console.log(person.hobbies); //Output: ["Writing", "coding", "swimming"]
+let strPerson = JSON.stringify(person); //Explanation: JSON.stringify() method converts a JavaScript object or value to a JSON string
+console.log(strPerson); //Output: {"firstName":"John","lastName":"Doe","age":25}
