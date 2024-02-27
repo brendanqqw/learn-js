78bfe2b430a0bbcd131476d0f8789ed5702ab78f
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-27 14:34:24 +0800

    v11.6

---
+++ LearnJS11JSON/js/challenge.js
@@ -0,0 +1,32 @@
+let people = [
+  {
+    firstName: "Brendan",
+    lastName: "Degrano",
+    age: 22,
+  },
+  {
+    firstName: "Brandon",
+    lastName: "Degrano",
+    age: 15,
+  },
+  {
+    firstName: "Ira",
+    lastName: "Degrano",
+    age: 9,
+  },
+  {
+    firstName: "Brenda",
+    lastName: "Degrano",
+    age: 45,
+  },
+  {
+    firstName: "Raul",
+    lastName: "Degrano",
+    age: 50,
+  },
+];
+for (let i = 0; i < people.length; i++) {
+  console.log(`First Name : ${people[i].firstName}`);
+  console.log(`Last Name: ${people[i].lastName}`);
+  console.log(`Age: ${people[i].age}`);
+}
