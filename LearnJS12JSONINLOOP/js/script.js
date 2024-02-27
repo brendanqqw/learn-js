4ffc122e5bedc6c301823dc7f0a1cc6cdb543645
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-27 15:43:50 +0800

    v12.2

--- LearnJS12JSONINLOOP/js/script.js
+++ LearnJS12JSONINLOOP/js/script.js
@@ -4,8 +4,7 @@ let person = {
   age: 22,
 };
 
-let keys = Object.keys(person); //Object.keys() is a method that returns an array of a given object's own property names, in the same order as we get with a normal loop.
-console.log(keys); //Explanation: This will return an array of the keys in the object person.
-
-//Output:
-// [ 'firstName', 'lastName', 'age' ]
+let keys = Object.keys(person);
+for (let i = 0; i < keys.length; i++) {
+  console.log(person[keys[i]]);
+}
