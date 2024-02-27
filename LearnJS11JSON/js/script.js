f4e42b6a7d2ba6a04ca569fab8c6cbb819233a98
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-27 12:26:14 +0800

    v11.5

--- LearnJS11JSON/js/script.js
+++ LearnJS11JSON/js/script.js
@@ -2,14 +2,20 @@ let person = {
   firstName: "John",
   lastName: "Doe",
   age: 25,
+  hobbies: ["reading", "coding", "swimming"],
 };
 
 //Update Existing "key | value"
 person["firstName"] = "Brendan"; //Explanation: adds a new key | value to the object
-//person.firstName = "Brendan"; //Explanation: adds a new key | value to the object
+//person.firstName = "Brendan"; Same Concept
 console.log(person); //Output: {firstName: "Brendan", lastName: "Doe", age: 25}
 
 //Create New Key | Value"
 person["middleInitial"] = "B"; //Explanation: adds a new key | value to the object
-person.middleInitial = "B"; //Explanation: adds a new key | value to the object
+person.middleInitial = "B"; // Same Concept
 console.log(person); //Output: {firstName: "Brendan", lastName: "Doe", age: 25, middleInitial: "B"}
+
+console.log(person.hobbies); //Output: ["reading", "coding", "swimming"]
+person.hobbies[0] = "Writing";
+//person.hobbies[0] = "Playing" //Same Concept
+console.log(person.hobbies); //Output: ["Writing", "coding", "swimming"]
