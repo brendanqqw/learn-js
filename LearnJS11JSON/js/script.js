fb654f7246e6f8350ec4b6dc4e422f270b47afc5
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-27 12:19:12 +0800

    v11.5

--- LearnJS11JSON/js/script.js
+++ LearnJS11JSON/js/script.js
@@ -2,8 +2,12 @@ let person = {
   firstName: "John",
   lastName: "Doe",
   age: 25,
-  hobbies: ["Coding", "Eating", "Sleeping"],
 };
 
-console.log(person["hobbies"][0]); //Explanation: We can access the array elements using the index number. In this case, the index number is 0. So, it will print "Coding" in the console.
-console.log(person.hobbies[1]); //Explanation: We can also access the array elements using the dot notation. In this case, the index number is 1. So, it will print "Eating" in the console.
+//Update Existing "key | value"
+person["firstName"] = "Brendan";
+console.log(person);
+
+//Create New Key | Value"
+person["middleInitial"] = "B";
+console.log(person);
