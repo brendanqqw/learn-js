c82399195cf282a20ec327fa97e09ab608d8ac5b
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-27 10:32:33 +0800

    v11.1

--- LearnJS11JSON/js/script.js
+++ LearnJS11JSON/js/script.js
@@ -1,9 +1,17 @@
 let person = {
   firstName: "John",
   lastName: "Doe",
-  age: 25,
+  3: 25,
 };
 
 //Only Works on a "String" Key
 console.log(`${person.firstName} ${person.lastName}`);
 //Output: John Doe
+
+console.log(person.3);
+//Output error because it is not a string key
+
+console.log(person["3"]);
+//Output: 25 shows the value of the key 3 because it is a string key
+
+
